69d1b7be218329ba6bf4bb722288fa90270ad193
Ariel <126970535+Aryel-Mag@users.noreply.github.com>
2023-03-30 09:29:50 +0200

    Feature #4: creating a private page

---
+++ www/404.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <title>Page Introuvable</title>
+  </head>
+  <body>
+    <div class="text-center align-self-center m-5">
+      <div>
+        <h1>PAGE INTROUVABLE</h1>
+        <p>Nous sommes désolé, cette route n'existe pas ou est introuvable.</p>
+      </div>
+      <div>
+        <img src="./resources/broken-nike-logo.png" alt="logo-404" class="error-img-size" />
+      </div>
+
+      <a href="index.php" class="btn btn-primary"
+        >Retour à l'Accueil</a
+      >
+    </div>
+    
+    <!-- Boostrap link for Js -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+    ></script>
+    <!-- END JS LINK -->
+  </body>
+</html>
---
+++ www/content.php
@@ -0,0 +1,65 @@
+<?php
+   // start or resume a session --------
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Main Content</title>
+</head>
+<body>
+
+ <!-- APPEL LE HEADER DE LA PAGE -->
+<?php include "header.php"; ?>
+<?php
+   // test if the session was previously started ---
+   if(isset($_SESSION['nickname'])){
+      require "reader.php";
+?>
+<!-- création d'un tableau dynamique avec html qui affiche les données du fichier CSV -->
+<div class="container text-center align-self-center w-100 h-100">
+   <div class="row">
+      <div class="col-sm">
+         <div class="table-responsive">
+            <table class="table table-bordered">
+               <thead>
+                  <th>NOM</th>
+                  <th>PRÉNOM</th>
+                  <th>EMAIL</th>
+                  <th>MESSAGE</th>
+               </thead>
+               <tbody>
+               <!-- BOUCLE QUI AFFICHE TOUTES LES INFOS ET CREE UNE NOUVELLE LIGNE
+                  POUR CHAQUE UTILISATEUR -->
+               <?php
+                  for($i = 0; $i < $cntr - 1; $i++ ){
+               ?>   
+                  <tr>
+                     <td><?= $surname[$i] ?></td>
+                     <td><?= $name[$i] ?></td>
+                     <td><?= $email[$i] ?></td>
+                     <td><?= $message[$i] ?></td>
+                  </tr>
+                  
+               <?php
+               }
+               ?>
+               </tbody>
+            </table>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php
+   }
+   // if the session wasn't started it shows the error page
+   else{
+      include "404.php";
+   }
+?>
+<!-- APPEL LE FOOTER DE LA PAGE -->
+<?php include "footer.php"; ?>
+</body>
+</html>
