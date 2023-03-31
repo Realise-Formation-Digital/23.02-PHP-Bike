53c17db57c2ff173cfd2030e31b4aeb04409cea3
Mirziano <Spagnolo_mg@outlook.com>
2023-03-31 11:08:41 +0200

    corecction du header footer via include php

--- www/inscription.php
+++ www/inscription.php
@@ -42,8 +42,21 @@
         }
     }
 
-        include 'header.php';
     ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <!-- APPEL LE HEADER DE LA PAGE -->
+    <?php include "header.php"; ?>
+        
+    
     
     <h1>Inscription</h1>
 
@@ -64,3 +77,5 @@
 
 
 <?php include 'footer.php';?>
+</body>
+</html>
